ca53186291c75b8b953a3f42d323b6244bfb4252
vanireeya <reeya.vani@sjsu.edu>
2018-11-20 12:42:53 -0800

    added navbar

--- LinkedIn/frontend/src/components/Main.js
+++ LinkedIn/frontend/src/components/Main.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import homepage from './homepage';
+import navbar from './navbar';
 
 
 class Main extends Component {
     render() {
         return (
             <div>
-                
+
                 <Route exact path="/" component={homepage} />
+                <Route exact path="/navbar" component={navbar} />
                 {/* <Route path="/TravelerLogin" component={Login} /> */}
-                
-                
+
+
             </div>
         )
     }
