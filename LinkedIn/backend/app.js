851443a4062efa0d2b9a101ab88a3e79539133ae
Devanshi Trivedi <devanshi.trivedi@sjsu.edu>
2018-11-20 21:47:38 -0800

    Formatting

--- LinkedIn/backend/app.js
+++ LinkedIn/backend/app.js
@@ -23,8 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect(mongo.url,{
-  poolSize:mongo.pool
+mongoose.connect(mongo.url, {
+  poolSize: mongo.pool
 })
   .then(() => console.log("Connected"))
   .catch((err) => console.log(err))
@@ -32,15 +32,15 @@ mongoose.connect(mongo.url,{
 
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
-app.use('/job',jobsRouter)
+app.use('/job', jobsRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- LinkedIn/backend/routes/jobs.js
+++ LinkedIn/backend/routes/jobs.js
@@ -51,7 +51,7 @@ router.post("/", async function (req, res, next) {
                     }
                 }).exec()
                     .then(userResult => {
-                        console.log("User Updated ",userResult)
+                        console.log("User Updated ", userResult)
                         res.writeHead(200, {
                             'Content-Type': 'application/json'
                         })
@@ -82,38 +82,38 @@ router.post("/", async function (req, res, next) {
             res.end(JSON.stringify(data))
         })
 })
- 
-router.get("/:jobID", async function(req,res,next){
-    console.log("Inside get joblist.") 
+
+router.get("/:jobID", async function (req, res, next) {
+    console.log("Inside get joblist.")
     const jobID = req.params.jobID
-  
-    Job.find({"_id":jobID})
-    .exec()
-      .then(result => {
-        console.log("The received result is : ", result);
-        res.writeHead(200,{
-          'Content-Type':'application/json'
+
+    Job.find({ "_id": jobID })
+        .exec()
+        .then(result => {
+            console.log("The received result is : ", result);
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            })
+            const data = {
+                "status": 1,
+                "msg": "Successfully got job details",
+                "info": result
+            }
+            res.end(JSON.stringify(data))
         })
-        const data = {
-          "status":1,
-          "msg":"Successfully got job details",
-          "info": result
-        }
-        res.end(JSON.stringify(data))
-      })
-      .catch(err => {
-        res.writeHead(200,{
-          'Content-Type':'application/json'
+        .catch(err => {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            })
+            const data = {
+                "status": 0,
+                "msg": "No Such User",
+                "info": {
+                    "error": err
+                }
+            }
+            res.end(JSON.stringify(data))
         })
-        const data = {
-          "status":0,
-          "msg":"No Such User",
-          "info": {
-            "error":err
-          } 
-        }
-        res.end(JSON.stringify(data))
-      })
-  })
+})
 
 module.exports = router;
