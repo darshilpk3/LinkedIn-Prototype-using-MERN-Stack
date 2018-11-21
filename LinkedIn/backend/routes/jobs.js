02ee86d68e1d6c0a6998ffae48a9b74062b4ba77
DesaiKavina <kavina.desai@sjsu.edu>
2018-11-21 01:44:47 -0800

    search for job and edit job details route done

--- LinkedIn/backend/routes/jobs.js
+++ LinkedIn/backend/routes/jobs.js
@@ -86,18 +86,23 @@ router.post("/", async function (req, res, next) {
 router.post("/search", async function(req, res, next){
 
     console.log("\nInside the search request for jobs");
-    console.log("Request obtained is : ");
+    console.log("\nRequest obtained is : ");
     console.log(JSON.stringify(req.body));
 
     var searched_job_title = req.body.job_title
     var searched_job_location = req.body.location
 
+    //making the regex for the mongo query
     var splitted = searched_job_title.split(" ");
-    console.log(splitted);
+    var regex_str = "^(.*";
+    for(let i=0;i<splitted.length;i++)
+    {
+        regex_str = regex_str + splitted[i] + ".*";
+    }
+    regex_str = regex_str + ")$";
 
     Job.find({
-    //   jobTitle : {$regex : "/.*" + searched_job_title + ".*/i"},
-      jobTitle : {$regex : "^(.*" + "React" + ".*" + "Developer" + ".*)$"},      
+      jobTitle : {$regex : regex_str},     
       location : searched_job_location
     })
     .then((result,err) => {
@@ -121,6 +126,78 @@ router.post("/search", async function(req, res, next){
           const data = {
             "status":1,
             "msg":"Successfully fetched the search results",
+            "info": {
+              "result":result
+            }
+          }
+          res.end(JSON.stringify(data))
+        }
+    })
+    .catch(err => {
+        res.writeHead(400,{
+          'Content-Type':'application/json'
+        })
+        const data = {
+          "status":0,
+          "msg":"Backend Error",
+          "info": {
+            "error":err
+          } 
+        }
+        res.end(JSON.stringify(data))
+    })
+})
+
+router.put("/:jobId", async function(req, res, next){
+    console.log("\nInside the edit request for jobs");
+    console.log("\nRequest obtained is : ");
+    console.log(JSON.stringify(req.body));
+
+    var setJobId = req.params.jobId
+
+    var job_title = req.body.jobTitle
+    var job_description = req.body.description
+    var job_industry = req.body.industry
+    var employment_type = req.body.employmentType
+    var job_location = req.body.location
+    var job_function = req.body.jobFunction
+    var company_logo = req.body.companyLogo
+
+    Job.findByIdAndUpdate(setJobId,
+        {
+          $set:{
+            jobTitle: job_title,
+            description : job_description,
+            industry : job_industry,
+            employmentType : employment_type,
+            location : job_location,
+            jobFunction : job_function,
+            companyLogo : company_logo
+          }
+        }
+    )
+    .exec()
+    .then((result,err) => {
+        if(err){
+          res.writeHead(200,{
+            'Content-Type':'application/json'
+          })
+          const data = {
+            "status":0,
+            "msg":"No Such Data found",
+            "info": {
+              "error":err
+            }
+          }
+          res.end(JSON.stringify(data))  
+        }else{
+          res.writeHead(200,{
+            'Content-Type':'application/json'
+          })
+          console.log("\nJob details updated successfully");
+          const data = {
+            "status":1,
+            "msg":"Successfully updated the job details",
             "info": {
               "result":result
             }
